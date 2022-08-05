2aabcd1512eca1602a29da1bca6b7b16c30bce3f
Gokulraj0606 <108870590+Gokulraj0606@users.noreply.github.com>
2022-08-06 02:07:19 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,26 @@
-class Movie{
-  constructor(title,studio,rating){
-     this.title=title;
-     this.studio=studio;
-     this.rating="PG";
-  }
-  
-  getPG(){
-  return [this.rating];
-  }
-  set changetitle(modifiedtitle){
-    this.title=modifiedtitle;
-    }
-    set changestudio(modifiedstudio){
-    this.studio=modifiedstudio;
-    }
-    set changerating(modifiedrating){
-    this.rating=modifiedrating;
-    }
-  
- }
+class Circle{
+
+   constructor(radius,color){
+     this.radius=radius;
+     this.color=color;
+     }
+     getarea(){
+       return 3.1412*this.radius*this.radius
+     }
+     getcircumference(){
+       return 2*3.1412*this.radius;
+       }
+     
+     set changecolor(modifiedcolor){
+       this.color=modifiedcolor;
+       }
+   
+   }
  
- var a=new Movie("title","studio","rating");
- a.changetitle="Casino Royale"
- a.changestudio="Eon Productions"
- a.changerating="PG13"
- console.log(a.title);
- console.log(a.studio);
- console.log(a.rating);
+ 
+ 
+ var b=new Circle(1,"red");
+ b.changecolor="void";
+ console.log(b.color);
+ console.log(b.getarea());
+ console.log(b.getcircumference());
